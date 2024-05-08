47ba7670b134144160f94bf82c8364d97ad327ee
ice breaker <1324318532@qq.com>
2024-05-08 16:16:01 +0800

    ds

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,14 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import ds from './assets/ds.png'
 </script>
 
 <template>
   <div
     class="antialiased text-slate-500 dark:text-slate-400 bg-white dark:bg-slate-900 min-h-screen w-screen flex items-center justify-center">
     <div>
-      <div class="flex justify-between items-center">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" class="w-8" alt="Vite logo" />
-        </a>
-        <a href="https://vuejs.org/" target="_blank">
-          <img src="./assets/vue.svg" class="w-8" alt="Vue logo" />
-        </a>
-        <a href="https://tailwindcss.com/" target="_blank">
-          <img src="./assets/tailwindcss.svg" class="w-8" alt="TailwindCSS logo" />
-        </a>
-      </div>
+      <img :src="ds">
 
-      <HelloWorld msg="Vite + Vue + TailwindCSS" />
+
     </div>
 
   </div>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import './style.css'
 import App from './App.vue'
+import type { AnchorHTMLAttributes } from 'vue'
+
+export type xx = AnchorHTMLAttributes
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
