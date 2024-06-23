964e56ca29b5b2c62e337e408ba2fd920a62594f
ice breaker <1324318532@qq.com>
2024-06-23 15:44:13 +0800

    feat: commiy tw patch

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,18 @@
 <script setup lang="ts">
 import { useStore } from '../store'
 
-const store = useStore()
 defineProps<{ msg: string }>()
+const store = useStore()
 </script>
 
 <template>
-  <div class="text-center space-y-2">
+  <div class="space-y-2 text-center">
     <h1>{{ msg }}</h1>
 
     <div>
-      <button type="button" @click="store.increment()">count is {{ store.count }}</button>
+      <button type="button" @click="store.increment()">
+        count is {{ store.count }}
+      </button>
       <p>
         Double count {{ store.double }}
       </p>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue2'
+import utwm from 'unplugin-tailwindcss-mangle/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), utwm({
+    classMapOutput: true,
+  })],
 })
