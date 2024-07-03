2759fdf512b727ab8819d0693b49885e7b8542f9
ice breaker <1324318532@qq.com>
2024-07-03 22:02:30 +0800

    chore(deps): upgrade

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue2'
-import utwm from 'unplugin-tailwindcss-mangle/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), utwm({
-    classMapOutput: true,
-  })],
+  plugins: [vue()],
 })
