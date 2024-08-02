3b58bd1d203e34dced09fd650c60f48ca3378514
ice breaker <1324318532@qq.com>
2024-08-02 18:00:42 +0800

    xxx

--- src/router.ts
+++ src/router.ts
@@ -3,6 +3,7 @@ import type { RouteConfig } from 'vue-router'
 import VueRouter from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 import Demo from './components/Demo.vue'
+import Demo2 from './components/Demo2.vue'
 
 Vue.use(VueRouter)
 
@@ -10,6 +11,7 @@ export const routes: RouteConfig[] = [
   { path: '/', component: HelloWorld, props: { msg: 'Home' } },
   { path: '/bar', component: HelloWorld, props: { msg: 'Bar' } },
   { path: '/demo', component: Demo, props: { msg: 'Bar' } },
+  { path: '/demo2', component: Demo2, props: { msg: 'Bar' } },
 ]
 
 const router = new VueRouter({
