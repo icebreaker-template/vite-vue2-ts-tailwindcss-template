31e2800c4a473b406f18446fb611b22e686a9dae
ice breaker <1324318532@qq.com>
2024-08-14 18:17:03 +0800

    fsda

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,73 @@
 <script setup lang="ts">
+import Dexie, { type EntityTable } from 'dexie'
 import { useStore } from '../store'
 import tailwindcssIcon from '../assets/tailwindcss.svg'
 
 defineProps<{ msg: string }>()
+
+interface Friend {
+  id: number
+  name: string
+  age: number
+}
+
+const db = new Dexie('dexie-db') as Dexie & {
+  friends: EntityTable<
+    Friend,
+    'id' // primary key "id" (for the typings only)
+  >
+  productInventoryCollection: EntityTable<
+    Friend,
+    'id' // primary key "id" (for the typings only)
+  >
+}
+
+console.log('Dexie.js phnx db initialized')
+
+// schema definition syntax - http://dexie.org/docs/Version/Version.stores()
+// NOTE: New versions MUST be placed after existing versions due to logic in Dexie
+// db.version(1).stores({
+//   products: 'id, defaultPrices.retailPrice.price, depth, description, express_item, height, length, name, sku, weight, brand',
+// })
+
+db.version(2).stores({
+  products: 'id, brand, defaultPrices.retailPrice.price, *edges.category, sku,ooo,ccc',
+})
+
+db.version(4).stores({
+  products: 'id, brand, defaultPrices.retailPrice.price, *edges.category, sku,ooo,ddd,oaoa',
+})
+
+db.version(3).stores({
+  productsCollection: 'id, brand, defaultPrices.retailPrice.price, *edges.category, sku',
+  productInventoryCollection: 'productId',
+})
+
+db.version(4).stores({
+  productsCollection: 'id, brand, defaultPrices.retailPrice.price, *edges.category, sku',
+  productOptionsCollection: 'variantKey',
+  productInventoryCollection: 'productId',
+})
+
+console.log(db.productInventoryCollection.add({
+  productId: '11',
+}))
+// @ts-ignore
+console.log(db.products.add({
+  id: '11',
+  ccc: 111,
+}))
+
+// db.friends.
+
+// @ts-ignore
+// db.products.bulkPut([
+//   {
+//     id: 1,
+//   },
+
+// ])
+
 const store = useStore()
 </script>
 
