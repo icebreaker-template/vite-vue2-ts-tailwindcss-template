a18dcf027c7f30d2cd6abbc021df2939d6611ea8
ice breaker <1324318532@qq.com>
2025-02-11 13:10:48 +0800

    chore(deps): upgrade

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,23 +1,15 @@
 <script setup lang="ts">
-import { useStore } from '../store'
+
 import tailwindcssIcon from '../assets/tailwindcss.svg'
 
 defineProps<{ msg: string }>()
-const store = useStore()
+
 </script>
 
 <template>
   <div class="space-y-2 text-center">
     <h1>{{ msg }}</h1>
     <img class="w-8" :src="tailwindcssIcon">
-    <div>
-      <button class="bg-pink-100" type="button" @click="store.increment()">
-        count is {{ store.count }}
-      </button>
-      <p class="text-yellow-800">
-        Double count {{ store.double }}
-      </p>
-    </div>
   </div>
 </template>
 
