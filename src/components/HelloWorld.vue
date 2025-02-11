c427dddc0c83ba1a824961faa9bd5f733b4ca095
ice breaker <1324318532@qq.com>
2025-02-11 13:27:30 +0800

    chore: x

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,75 @@
-<script setup lang="ts">
-
-import tailwindcssIcon from '../assets/tailwindcss.svg'
-
-defineProps<{ msg: string }>()
-
+<script lang="ts" setup>
+const tableData = Array.from({ length: 100 }).map(() => {
+  return {
+    date: '2016-05-02',
+    name: '王小虎',
+    province: '上海',
+    city: '普陀区',
+    address: '上海市普陀区金沙江路 1518 弄',
+    zip: 200333,
+  }
+})
 </script>
 
 <template>
-  <div class="space-y-2 text-center">
-    <h1>{{ msg }}</h1>
-    <img class="w-8" :src="tailwindcssIcon">
+  <div class="container mx-auto">
+    <div class="w-160">
+      <el-table class="sticky-x" :data="tableData">
+        <el-table-column prop="date" label="日期" width="150" />
+        <el-table-column prop="name" label="姓名" width="120" />
+        <el-table-column prop="province" label="省份" width="120" />
+        <el-table-column prop="city" label="市区" width="120" />
+        <el-table-column prop="address" label="地址" width="300" />
+        <el-table-column prop="zip" label="邮编" width="120" />
+
+        <el-table-column prop="city" label="市区" width="120" />
+        <el-table-column prop="address" label="地址" width="300" />
+        <el-table-column prop="zip" label="邮编" width="120" />
+        <el-table-column prop="city" label="市区" width="120" />
+        <el-table-column prop="address" label="地址" width="300" />
+        <el-table-column prop="zip" label="邮编" width="120" />
+        <el-table-column label="操作" width="100">
+          <template>
+            <el-button type="text" size="small">
+              查看
+            </el-button>
+            <el-button type="text" size="small">
+              编辑
+            </el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+
+    <div class="mt-100">
+      1
+    </div>
   </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.el-table.sticky-x {
+  position: static;
+  overflow: initial;
+
+  .el-table__body-wrapper {
+    /* position: static;
+    overflow: initial; */
+
+    .el-scrollbar {
+      position: static;
+      overflow: initial;
+
+      .el-scrollbar__bar.is-horizontal {
+        position: sticky;
+        bottom: 20px !important;
+        height: 0px;
+
+        .el-scrollbar__thumb {
+          height: 6px;
+        }
+      }
+    }
+  }
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
+import ElementUI from 'element-ui'
 import Vue from 'vue'
-import './style.css'
-
 import App from './App.vue'
 import router from './router'
+import './style.css'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
-
+Vue.use(ElementUI)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
