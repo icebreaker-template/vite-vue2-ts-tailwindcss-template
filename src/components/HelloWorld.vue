43e2c5cdaa76a7612152dc7a104931f69f023ce3
ice breaker <1324318532@qq.com>
2025-05-08 10:01:06 +0800

    chore(deps): upgrade

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { onMounted } from 'vue'
+import { RouterLink } from 'vue-router'
 import tailwindcssIcon from '../assets/tailwindcss.svg'
 
 defineProps<{ msg: string }>()
@@ -11,6 +12,12 @@ onMounted(() => {
 
 <template>
   <div class="space-y-2 text-center">
+    <RouterLink active-class="bg-[#123456]" exact class="text-blue-400" to="/">
+      首页
+    </RouterLink>
+    <RouterLink active-class="bg-[#123456]" exact class="text-blue-400" to="/bar">
+      bar
+    </RouterLink>
     <h1>{{ msg }}</h1>
     <img class="w-8" :src="tailwindcssIcon">
   </div>
